df568664a2dd755b74b7d998ced14347a0ca4cc5
yzf750 <yzf750@gmail.com>
2024-02-05 08:11:49 -0500

    Update snmp-scan.sh

--- snmp-scan/snmp-scan.sh
+++ snmp-scan/snmp-scan.sh
@@ -3,27 +3,41 @@
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <CIDR> <COMMUNITY_STRING>"
     echo "Example: $0 192.168.1.0/24 public"
-    echo "Example: $0 192.168.1.0/24 private"
     exit 1
 fi
 
 CIDR=$1
 COMMUNITY_STRING=$2
 
+# ANSI escape codes for text formatting
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 # Function to check if an IP has SNMP enabled
 check_snmp() {
     IP=$1
     SYS_DESCR=$(snmpwalk -v2c -c $COMMUNITY_STRING $IP 1.3.6.1.2.1.1.1.0 2>/dev/null | awk -F ': ' '{print $2}')
     if [ $? -eq 0 ] && [ -n "$SYS_DESCR" ]; then
-        echo "Default SNMP community string $COMMUNITY_STRING is enabled on IP address $IP - System Description: $SYS_DESCR"
+
+		# Use color output
+		# echo -e "SNMP is enabled on ${RED}$IP${NC} - System Description: $SYS_DESCR - Community String: ${GREEN}$COMMUNITY_STRING${NC}"
+		
+		# No color output
+		echo -e "SNMP is enabled on $IP - System Description: $SYS_DESCR - Community String: $COMMUNITY_STRING"
+        echo "$IP" > $COMMUNITY_STRING-affected-ips.txt
     fi
 }
 
 # Loop through the IP addresses in the CIDR range
-echo ""
 for IP in $(nmap -sn $CIDR | grep 'Nmap scan report' | awk '{print $5}'); do
     check_snmp $IP &
 done
 
 # Wait for all background processes to finish
 wait
+
+# Display the list of affected IP addresses
+echo -e "\nList of affected IP addresses:"
+cat $COMMUNITY_STRING-affected-ips.txt
+# rm -f affected_ips.txt
