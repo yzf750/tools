975d5853ae8cd3b13acf4c8127c4512ad97e57e8
yzf750 <yzf750@gmail.com>
2024-02-06 08:22:32 -0500

    Update snmp-scan.sh

--- snmp-scan/snmp-scan.sh
+++ snmp-scan/snmp-scan.sh
@@ -2,7 +2,12 @@
 
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <CIDR> <COMMUNITY_STRING>"
-    echo "Example: $0 192.168.1.0/24 public"
+	echo ""
+	echo "Single Subnet"    
+	echo "Example: $0 192.168.1.0/24 public"
+	echo ""	
+	echo "Multiple Subnets"
+	echo "Example: $0 192.168.1.0/24,10.10.10.0/24 public"
     exit 1
 fi
 
@@ -16,29 +21,40 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 # Function to check if an IP has SNMP enabled
-echo -e ""
+echo ""
 check_snmp() {
     IP=$1
     SYS_DESCR=$(snmpwalk -v2c -c $COMMUNITY_STRING $IP 1.3.6.1.2.1.1.1.0 2>/dev/null | awk -F ': ' '{print $2}')
     if [ $? -eq 0 ] && [ -n "$SYS_DESCR" ]; then
 
-	# Use color output
-	echo -e "SNMP community string ${GREEN}'$COMMUNITY_STRING${NC}' is enabled on ${RED}$IP${NC}"
-	echo -e "System Description: $SYS_DESCR"
-	echo -e ""
+		# Use color output
+		echo -e "SNMP community string ${GREEN}'$COMMUNITY_STRING${NC}' is enabled on ${RED}$IP${NC}"
+		echo -e "System Description: $SYS_DESCR"
+		echo -e ""
 		
-	# No color output
-	#echo -e "SNMP community string '$COMMUNITY_STRING' is enabled on $IP"
-	#echo -e "System Description: $SYS_DESCR"
- 	#echo -e ""
+		# No color output
+		#echo -e "SNMP community string '$COMMUNITY_STRING' is enabled on $IP"
+		#echo -e "System Description: $SYS_DESCR"
+		#echo -e ""
 
         echo "$IP" >> $CIDR_CLEANED-$COMMUNITY_STRING-affected-ips.txt
     fi
 }
 
 # Loop through the IP addresses in the CIDR range
-for IP in $(nmap -sn $CIDR | grep 'Nmap scan report' | awk '{print $5}'); do
-    check_snmp $IP &
+#for IP in $(nmap -sn $CIDR | grep 'Nmap scan report' | awk '{print $5}'); do
+#    check_snmp $IP &
+#done
+
+# Split the comma-separated CIDR list into an array
+IFS=',' read -ra CIDR_ARRAY <<< "$CIDR"
+
+# Loop through the provided CIDR ranges
+for CIDR in "${CIDR_ARRAY[@]}"; do
+    # Loop through the IP addresses in the CIDR range
+    for IP in $(nmap -sn $CIDR | grep 'Nmap scan report' | awk '{print $5}'); do
+        check_snmp $IP &
+    done
 done
 
 # Wait for all background processes to finish
@@ -47,5 +63,5 @@ wait
 # Display the list of affected IP addresses
 echo -e "\nList of affected IP addresses:"
 cat $CIDR_CLEANED-$COMMUNITY_STRING-affected-ips.txt
-echo -e ""
+echo ""
 # rm -f $COMMUNITY_STRING-affected_ips.txt
